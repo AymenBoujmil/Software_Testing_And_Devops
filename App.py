501bc834d113061b6891bc80a0bcc466b0bd8288
AymenBoujmil <56639521+AymenBoujmil@users.noreply.github.com>
2022-06-07 17:01:32 +0100

    cd deployment

--- App.py
+++ App.py
@@ -132,7 +132,7 @@ def create_app():
 def main():
     app = create_app()
     app.secret_key = 'flaskSecret'
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
